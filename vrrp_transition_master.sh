bd8f8d0bcbb5d261a293b915d628ea4ed34a6d37
magnumit-nz <mark@magnumit.co.nz>
2019-09-19 11:46:28 +1200

    Create vrrp_transition_master.sh

---
+++ vrrp_transition_master.sh
@@ -0,0 +1,41 @@
+
+#!/bin/bash
+touch /var/log/vrrp.log
+echo "`date` Route Check: on $1 using $2 status $3 " >> /var/log/vrrp.log
+
+#Set to LAN Interface (one VRRP is running on)
+interface=eth5
+
+#set to group number used on VRRP group
+group=100
+
+#use different values for primary and secondary routers.
+#inactive on primary should be higher than active of seconary.
+#eg) Primary active = 200,  Secondary active = 100
+#    Primary inactive= 150, Secondary inactive = 50
+activepriority=200
+inactivepriority=150
+
+WLBStatus=$3
+echo $WLBStatus
+#start config change
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
+
+if [ $WLBStatus = 'inactive' ]
+then
+  newpriority=$inactivepriority
+elif [ $WLBStatus = 'active' ]
+then
+  newpriority=$activepriority
+fi
+echo $newpriority
+
+# change priority
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces ethernet eth5 vrrp vrrp-group 100 priority $newpriority
+
+# now commit the changes
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
+
+# finish configuring
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
+echo "`date` VRRP Priority = $newpriority " >> /var/log/vrrp.log
